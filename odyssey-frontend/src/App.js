a7477ae23187ec27f4a68972d242559aface5699
Sanika <sanikaardekar@gmail.com>
2022-02-13 09:18:25 +0530

    changes in file structure, relative interface added

--- odyssey-frontend/src/App.js
+++ odyssey-frontend/src/App.js
@@ -9,14 +9,16 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Create from "./Pages/Admin/Create";
-import View from "./Pages/Admin/View";
-import Enrolled from "./Pages/Admin/Enrolled";
-import Verify from "./Pages/Admin/Verify";
-import PatientDetails from './Pages/Admin/PatientDetails';
+import Create from "./Interfaces/Admin/Create";
+import View from "./Interfaces/Admin/View";
+import Enrolled from "./Interfaces/Admin/Enrolled";
+import Relative from './Interfaces/Relative/Relative';
+import Verify from "./Interfaces/Admin/Verify";
+import PatientDetails from './Interfaces/Admin/PatientDetails';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import SignupForRelative from "./Auth/Signup/SignupForRelative";
 function App() {
 
   useEffect(() => {
@@ -30,11 +32,13 @@ function App() {
         <Route exact path="/create" element={<Create />} />
         <Route exact path="/view" element={<View />} />
         <Route exact path="/enrolled" element={<Enrolled />} />
-        <Route exact path="/patient/:id" render={(props)=>{
-        return( <PatientDetails {...props} /> )}} />
+        <Route exact path="/relative" element={<Relative />} />
+        {/* <Route exact path="/patient/:id" render={(props)=>{
+        return( <PatientDetails {...props} /> )}} /> */}
         {/* <Route exact path="/Verify" element={<Verify />} /> */}
         <Route exact path="/signupforadmin" element={<SignupPageAdmin />} />
-        <Route exact path="/signupforrelative" element={<SignupPageRelative />} />
+        <Route exact path="/signupforrelative" render={(props)=>{
+        return( <SignupPageRelative {...props} /> )}} />
         <Route exact path="/loginforadmin" element={<LoginPageAdmin />} />
         <Route exact path="/loginforrelative" element={<LoginPageRelative />} />
         <Route path="*" element={<NotFound />} />
