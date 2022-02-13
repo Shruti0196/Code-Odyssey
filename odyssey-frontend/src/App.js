bedacbdeeb2639724c9896b43926ff8ffe31653f
Rachana1104 <y.rachana1104@gmail.com>
2022-02-13 09:32:07 +0530

    enrolled

--- odyssey-frontend/src/App.js
+++ odyssey-frontend/src/App.js
@@ -9,13 +9,16 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Create from "../src/Interfaces/Admin/Create";
-import View from "../src/Interfaces/Admin/View";
-import Enrolled from "../src/Interfaces/Admin/Enrolled";
-import Verify from "../src/Interfaces/Admin/Verify";
+import Create from "./Interfaces/Admin/Create";
+import View from "./Interfaces/Admin/View";
+import Enrolled from "./Interfaces/Admin/Enrolled";
+import Relative from './Interfaces/Relative/Relative';
+import Verify from "./Interfaces/Admin/Verify";
+import PatientDetails from './Interfaces/Admin/PatientDetails';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import SignupForRelative from "./Auth/Signup/SignupForRelative";
 function App() {
 
   useEffect(() => {
@@ -29,9 +32,13 @@ function App() {
         <Route exact path="/create" element={<Create />} />
         <Route exact path="/view" element={<View />} />
         <Route exact path="/enrolled" element={<Enrolled />} />
-        <Route exact path="/verify" element={<Verify />} />
+        <Route exact path="/relative" element={<Relative />} />
+        {/* <Route exact path="/patient/:id" render={(props)=>{
+        return( <PatientDetails {...props} /> )}} /> */}
+        {/* <Route exact path="/Verify" element={<Verify />} /> */}
         <Route exact path="/signupforadmin" element={<SignupPageAdmin />} />
-        <Route exact path="/signupforrelative" element={<SignupPageRelative />} />
+        <Route exact path="/signupforrelative" render={(props)=>{
+        return( <SignupPageRelative {...props} /> )}} />
         <Route exact path="/loginforadmin" element={<LoginPageAdmin />} />
         <Route exact path="/loginforrelative" element={<LoginPageRelative />} />
         <Route path="*" element={<NotFound />} />
