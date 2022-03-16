5a4884b9180520f8ebd4fd984411e35d63fa170a
Sanika <sanikaardekar@gmail.com>
2022-03-16 23:52:19 +0530

    ui changes

--- src/Pages/LoginSignup/SignupPageAdmin.js
+++ src/Pages/LoginSignup/SignupPageAdmin.js
@@ -1,8 +1,8 @@
 import "./LoginSignup.css";
 import { Grid } from "@mui/material";
 import illustration2 from "../../Assets/illustration2.svg";
-import blueDots from "../../Assets/blueDots.gif";
-import orangeDots from "../../Assets/orangeDots.gif";
+//import blueDots from "../../Assets/blueDots.gif";
+//import orangeDots from "../../Assets/orangeDots.gif";
 import SignupForAdmin from "../../Auth/Signup/SignupForAdmin";
 import HeaderLogin from "../../components/Header/HeaderLogin";
 export default function SignupPageAdmin() {
@@ -10,9 +10,9 @@ export default function SignupPageAdmin() {
     <>
       <Grid container direction="row">
         <Grid item sm={6} md={3} className="right-box">
-          <div className="top-left">
+          {/* <div className="top-left">
             <img src={blueDots} alt="loading" className="dots" />
-          </div>
+          </div> */}
           <Grid
             item
             display="flex"
@@ -21,25 +21,25 @@ export default function SignupPageAdmin() {
           >
             <img src={illustration2} alt="signup" className="illustration" />
           </Grid>
-          <div className="bottom-right">
+          {/* <div className="bottom-right">
             <img src={blueDots} alt="loading" className="dots" />
-          </div>
+          </div> */}
         </Grid>
         <Grid item sm={6} md={6} className="left-box">
           <Grid xs={12} sm={6} className="links">
             <HeaderLogin activePage="Signup" />
           </Grid>
-          <div className="bottom">
+          {/* <div className="bottom">
             <img src={orangeDots} alt="loading" className="dots" />
           </div>
           <div className="top-right">
             <img src={orangeDots} alt="loading" className="dots" />
-          </div>
+          </div> */}
           <div className="box">
             <SignupForAdmin />
-            <div className="middle">
+            {/* <div className="middle">
               <img src={orangeDots} alt="loading" className="dots" />
-            </div>
+            </div> */}
           </div>
         </Grid>
       </Grid>
